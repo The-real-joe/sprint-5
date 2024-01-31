3e71e9c3623e2b1ebd1a7895fceded0d860156b2
The-real-joe <j.boucher690@gmail.com>
2024-01-31 10:51:51 -0500

    stuck

--- frontend/index.js
+++ frontend/index.js
@@ -1,6 +1,56 @@
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
 
+function cardMaker (user , mentor){
+  const userCard = document.createElement('div');
+  const userName = document.createElement('h3');
+  const email = document.createElement('div');
+  const mentorCard = document.createElement('h4');
+mentorName = document.createElement('ul');
+
+
+  userCard.classList.add('user-card');
+  email.classList.add('email');
+  mentorName.classList.add('mentor-name');
+
+  userCard.appendChild(userName);
+  userCard.appendChild(email);
+  userCard.appendChild(mentorCard);
+  mentorCard.appendChild(mentorName);
+
+
+  userCard.addEventListener('click', () => {
+    userCard.classList.toggle('selected')
+})
+mentorCard.addEventListener('click', () => { 
+  mentorCard.classList.toggle('selected')
+})
+userCard.textContent=learners.fullName, ID
+email.textContent=learners.email
+mentorCard.textContent= 'Mentors'
+mentorName.textContent= mentors.fullName 
+
+return userCard
+}
+function getUsers (){
+  axios.get('http://localhost:3003/api/learners')
+.then(res => {
+  const learners = res.data
+  const userCards = learners.map(learner => cardMaker(learner))
+ userCards.forEach(userCard => document.body.appendChild(userCard))
+})
+}
+function getMentors (){
+  axios.get('http://localhost:3003/api/mentors')
+.then(res=> {
+  const mentors = res.data
+  const mentorCards = mentors.map(mentor => cardMaker(mentor))
+  mentorCards.forEach(mentorCard => document.body.appendChild(mentorCard))
+})
+}
+
+
+
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
